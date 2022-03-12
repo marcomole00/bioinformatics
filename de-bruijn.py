aa51f389887d38f8b96714bbaf5519ada1dbd0e6
marcomole00 <marcomole00@gmail.com>
2022-03-12 13:05:57 +0100

    added hierholzer's algorithm

--- de-bruijn.py
+++ de-bruijn.py
@@ -1,3 +1,4 @@
+from platform import node
 import re
 import sys
 
@@ -12,13 +13,13 @@ def euler_theorem():
 
     if odd_degree_nodes == 0:
         print("an eulerian CYCLE exists")
-        return True
+        return 2
     elif odd_degree_nodes == 2:
         print("an eulerian PATH exists")
-        return True
+        return 1
     else:
         print("this graph is not traversable")
-        return False
+        return 0
 
 def generate_dot_graph(filename):
     f = open(f"{filename}.dot", "w")
@@ -40,12 +41,51 @@ def generate_dot_graph(filename):
 
 
 def hierholzer():
-    
+    if euler_theorem() == 0:
+        return
+
     tpath = [] #temporary stack
     epath = [] # where the eulerian path will be at the end
+
+    not_visited_edges = []
+    for key in nodes.keys():
+        for el in nodes[key]:
+            not_visited_edges.append((key, el))
     
+    #choose a suitable vertex -> start at the vertex with one more outgoing edge,
+    #  end at the vertex with one more incoming edge!
+    outgoing_edges = 0
+    for key in nodes.keys():
+        if len(nodes[key]) > outgoing_edges:
+            outgoing_edges = len(nodes[key])
+            v = key
+
+    tpath.append(v)
+    
+    while(len(tpath)>0):
+        u = tpath[-1]
+        already_in_visited = 0
+        # all outgoing edges from u have been visited?
+        for edges in nodes[u]:
+            if (u,edges) not in not_visited_edges: already_in_visited+=1
+        
+        # yes: pop u from tpath and push it to epath
+        if already_in_visited == len(nodes[u]):
+            epath.append(tpath.pop())
+        
+        # no: select a random edge (u,x), push x to tpath and mark (u,x) as visited       
+        else:
+            for x in nodes[u]:
+                if (u,x)  in not_visited_edges: 
+                    tpath.append(x)
+                    not_visited_edges.remove((u,x))
+                    break
+    
+    epath.reverse()
+    print(epath)
 
 
+            
 
 
 
@@ -53,6 +93,7 @@ def hierholzer():
 # $ dot -Tsvg {filename} > {output_name.svg}
 genome = input("enter the sequence: ")
 genome = re.sub('[^ACGT]', '', genome)
+if len(genome) == 0: genome = 'TCATTCTTCAGGTCAAA'
 print(f"here is the genome: {genome}")
 
 k_mer_size = int(input("enter the k-mer size:"))
@@ -77,8 +118,9 @@ for i in range(0,len(genome)-k_mer_size+1):
 
     nodes[genome[i:i+k_mer_size-1]].append(genome[i+1:i+k_mer_size])
     
-    nodes_degree[genome[i:i+k_mer_size-1]]-=1
     
+    #degree is degree_in - degree_out 
+    nodes_degree[genome[i:i+k_mer_size-1]]-=1
     nodes_degree[genome[i+1 : i+k_mer_size]]+=1
 
 
@@ -89,3 +131,4 @@ else:
     generate_dot_graph("default_name_for_graph")
 
 euler_theorem()
+hierholzer()
